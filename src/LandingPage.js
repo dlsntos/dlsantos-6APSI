66e348ddf4c524dfad1027b159a66f0664491c07
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 03:40:18 +0800

    feat: add filler content for landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,7 +5,8 @@ import './LandingPage.css';
 function LandingPage() {
   return (
     <div className="landing-page-container">
-      
+        <h1>Im Landing Page</h1>
+      <div className='img-container'></div>
     </div>
   );
 }
