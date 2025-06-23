0a309442df8a0030c1d4a631307446640c2acc32
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-23 22:50:03 +0800

    feat: add new div container to group and center

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,20 +4,22 @@ function LoginPage() {
  
   return (
     <div>
-      <form onSubmit=''>
-        <h2>Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          required
-        />
-        <button >Login</button>
-      </form>
+      <div className='login-container'>
+        <form onSubmit=''>
+          <h2>Login</h2>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+          />
+          <button >Login</button>
+        </form>
+      </div>
     </div>
   );
 }
