6287450561e21af4c8fb1328f9df9ed90544cec7
SDGY <154787650+dlsntos@users.noreply.github.com>
2025-06-24 03:10:58 +0800

    feat: add empty div with classnName 'landing-page-container'

---
+++ src/LandingPage.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      
+    </div>
+  );
+}
+
+export default LandingPage;
